e28e4f361630929fd147f763a2cc84cd219b017d
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-04-19 23:41:01 +0100

    FIFO cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,8 +20,8 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
             discard_key = next(iter(self.cache_data))
-            print("DISCARD: ", discard_key)
             del (self.cache_data[discard_key])
+            print("DISCARD: ", discard_key)
 
     def get(self, key):
         """Get an item from cache"""
